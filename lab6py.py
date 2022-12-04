ed3213d90f455edb88f9ecdbcab787f21437d1c3
dwiupb1 <dwiupb1@gmail.com>
2022-12-04 21:25:57 +0700

    Pratikum6

--- lab6py.py
+++ lab6py.py
@@ -1,61 +1,21 @@
-nama = []
-nim = []
-tugas = []
-uts = []
-uas = []
+import math
 
-while True:
-    nama_input = input("Nama  : ")
-    nim_input = input("Nim   : ")
-    tugas_input = int(input("Tugas : "))
-    uts_input = int(input("UTS   : "))
-    uas_input = int(input("UAS   : "))
-    tanya = input("Tambah data(y/t)? ")
-    nama.append(nama_input)
-    nim.append(nim_input)
-    tugas.append(tugas_input)
-    uts.append(uts_input)
-    uas.append(uas_input)
-    if tanya == 't':
-        break
-print("""
-=====================================================================
-| No |     Nama      |    NIM    |  Tugas  |  UTS  |  UAS  |  Akhir |
-=====================================================================""")
+def a(x):
+    return  x**2
+a = lambda x : x**2
+print(a(2))
+def b(x, y):
+        return math.sqrt(x**2 + y**2)
 
-for i in range(len(nama)):
-    print("|",i+1,end="")
-    if i < 9 :
-        print("  |", end="")
-    else:
-        print(" |", end="")
+b = lambda x, y : x ** 2 + y ** 2
+print(b(2, 2))
+def c(*args):
+        return sum(args)/len(args)
 
-    print(" "+nama[i],end="")
-    for j in range(14-len(nama[i])):
-        print(" ",end="")
+c = lambda *args : sum(args)/len(args)
+print(c(1,2,3,4,5))
+def d(s):
+        return "".join(set(s))
 
-    print("|  "+nim[i],end="")
-    for k in range(9-len(nim[i])):
-        print(" ",end="")
-
-    print("|  "+str(tugas[i]),end="")
-    for l in range(7-len(str(tugas[i]))):
-        print(" ",end="")
-
-    print("|  "+str(uts[i]),end="")
-    for m in range(5-len(str(uts[i]))):
-        print(" ",end="")
-
-    print("|  "+str(uas[i]),end="")
-    for n in range(5-len(str(uas[i]))):
-        print(" ",end="")
-
-        akhir = round((float(tugas[i]*0.3)+float(uts[i]*0.35)+float(uas[i]*0.35)),2)
-
-    print("|  "+str(akhir),end="")
-    for o in range(6-len(str(akhir))):
-        print(" ",end="")
-
-    print("|")
-
-print("=====================================================================")
+d = lambda s: "".join(set(s))
+print(d("TUGAS"))
